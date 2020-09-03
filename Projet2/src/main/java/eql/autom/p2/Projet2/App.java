aad678a5dc2fe587e9abcd556212da80126ca757
Nicolas <nhubert@henix.fr>
2020-09-03 15:33:56 +0200

    ajout projet eclypse

---
+++ Projet2/src/main/java/eql/autom/p2/Projet2/App.java
@@ -0,0 +1,13 @@
+package eql.autom.p2.Projet2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
